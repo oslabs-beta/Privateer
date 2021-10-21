3246ef7bc5e70f82ef61e305b8f1dbba59b82361
dtjones404 <dustintaylorjones@gmail.com>
2021-10-21 11:35:54 -0400

    Created skeleton of monitoring page.

--- client/pages/Monitor.js
+++ client/pages/Monitor.js
@@ -1,15 +1,23 @@
-import { Container, Typography } from '@mui/material';
+import { Grid, Paper, Container } from '@mui/material';
 import React from 'react';
 
 const Monitor = () => {
   return (
-    <Container size="sm">
-      <Typography>
-        MONITORING PAGE MONITORING PAGE MONITORING PAGE MONITORING PAGE
-        MONITORING PAGE MONITORING PAGE MONITORING PAGE MONITORING PAGE
-        MONITORING PAGE MONITORING PAGE MONITORING PAGE MONITORING PAGE
-        MONITORING PAGE MONITORING PAGE
-      </Typography>
+    <Container maxWidth="false">
+      <Grid container spacing={3}>
+        <Grid item xs={12} lg={4}>
+          <Paper>1</Paper>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Paper>2</Paper>
+        </Grid>
+        <Grid item xs={12} lg={4}>
+          <Paper>3</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper>4</Paper>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
