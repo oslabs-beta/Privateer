7468c1f21d0f3fd81143dc3b135ec7c57a3c63fc
IsaiahDel516 <88635660+IsaiahDel516@users.noreply.github.com>
2021-11-01 11:42:44 -0400

    Revert "Isaiah/redux fix"

--- client/constants/actionTypes.js
+++
@@ -1,5 +0,0 @@
-// example
-// export const ADD_CARD = 'ADD_CARD';
-export const CONFIG_SAVE = 'CONFIG_SAVE';
-
-export const ADD_DATA = 'ADD_DATA';
--- server/routers/cluster.js
+++
@@ -1,9 +0,0 @@
-const clusterController = require('../controllers/clusterController');
-
-const router = require('express').Router();
-
-router.get('/pods', clusterController.getPods, (req, res) => {
-  res.status(200).json(res.locals.pods);
-});
-
-module.exports = router;
