ae694c2b8a4032487c049d65329ffa07ee5ef119
dtjones404 <dustintaylorjones@gmail.com>
2021-10-31 21:53:23 -0400

    Removed example code from actions.js.

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,18 +1,11 @@
 import * as types from '../constants/actionTypes';
 
-// example
-
-// export const updateLocation = data => ({
-//   type: types.UPDATE_LOCATION,
-//   payload: data,
-// });
-
-export const configMapSave = obj => ({
+export const configMapSave = (obj) => ({
   type: types.CONFIG_SAVE,
   payload: obj,
-})
+});
 
-export const addDataFields = num => ({
+export const addDataFields = (num) => ({
   type: types.ADD_DATA,
   payload: num,
-})
+});
