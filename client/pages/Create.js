fe6663688b03f1df5b408ffdb804b111b9813e4f
isaiah delgado <isaiah_del@yahoo.com>
2021-11-01 11:36:22 -0400

    re-organized state management. No longer using redux on deployment end, will now be switching over to react hooks. This is due to the nature of our deployment feature containing many forms which does not work well with the redux store.

--- client/pages/Create.js
+++ client/pages/Create.js
@@ -1,33 +1,15 @@
 import { Container, Typography } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import ConfigMap from '../components/create/ConfigMap';
 import DepServ from '../components/create/DepServ';
 import Secret from '../components/create/Secret';
 import WebApp from '../components/create/WebApp';
 import CenteredTabs from '../components/create/Header';
-import { connect } from 'react-redux';
 import * as actions from '../actions/actions';
 
-const mapStateToProps = ({ create }) => ({
-  //example
-  // newLocation: markets.newLocation,
-  cmApi: create.cmApi,
-  cmMetaName: create.cmMetaName,
-  cmDataNum: create.cmDataNum,
-  cmData: create.cmData,
-  
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  //example
-  // setLocation: (location) => dispatch(actions.setNewLocation(location)),
-  configSave: (obj) => dispatch(actions.configMapSave(obj)),
-  addData: (num) => dispatch(actions.addDataFields(num))
-});
-
 const Create = props => {
-  
+  const [cmState, setCmState] = useState({apiVersion: "", metaName: "", data:[[],[],[],[],[],[],[],[],[],[]], dataNum: 0})
   return (
     <Container size="sm">
       <Typography component={'span'} variant={'body2'}>
@@ -36,11 +18,11 @@ const Create = props => {
           <Switch>
             <Route path="/create/configmap">
               <ConfigMap 
-                cmApi={props.cmApi} 
-                cmMetaName={props.cmMetaName} 
-                cmDataNum={props.cmDataNum}   
-                cmData={props.cmData}
-                changeData={(num) => props.addData(num)}
+                cmApi={cmState.apiVersion} 
+                cmMetaName={cmState.metaName} 
+                cmDataNum={cmState.dataNum}   
+                cmData={cmState.data}
+                changeState={setCmState}
               />
             </Route>
             <Route path="/create/depserv">
@@ -59,4 +41,4 @@ const Create = props => {
   );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Create);
+export default Create;
