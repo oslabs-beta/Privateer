32d39c5cc82e0f1a809b3f8962324e00527080b0
IsaiahDel516 <88635660+IsaiahDel516@users.noreply.github.com>
2021-11-10 22:18:27 -0500

    Revert "Isaiah/logo"

--- preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-	ipcRenderer: {
-		chooseDir(...args) {
-			ipcRenderer.send('chooseDir', ...args)
-		}
-	}
-});
