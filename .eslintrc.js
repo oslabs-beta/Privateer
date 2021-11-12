1316492c91b5e1951242c67ed426a37cb11bfef9
dtjones404 <dustintaylorjones@gmail.com>
2021-11-12 14:07:08 -0500

    Updated eslintrc to be compatible with Prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     jest: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb'],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
