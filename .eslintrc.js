e91259fd85e6ffc8d62c69ba11203b6518acb956
dtjones404 <dustintaylorjones@gmail.com>
2021-11-12 12:18:09 -0500

    Resolved linting errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     node: true,
     jest: true,
-    // es2021: true,
+    es2021: true,
   },
   extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
